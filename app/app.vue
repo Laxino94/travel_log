36f7ac817481501031ba6d9a1154f4da30a058e6
Laxino94 <huzehua2013@gmail.com>
2025-10-04 18:58:18 +0800

    feat(ui and icons): ui and icons plugins (#2)
    * feat(ui and icons): ui and icons plugins
    
    * docs(mini change): minichange for cache lint ignore

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      hello world
+    </h1>
+    <button class="btn btn-secondary">
+      View Map <Icon name="tabler:map" size="24" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
   eslint: {
     config: {
       standalone: false // load config from .eslintrc (or package.json)
     },
     checker: true // check eslint on dev and build
+  },
+  css: ['@/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
   }
 })
