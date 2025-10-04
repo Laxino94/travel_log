7f853565b363e30a6dd8d18de330abb5efa32ddd
Laxino94 <huzehua2013@gmail.com>
2025-10-04 15:15:14 +0800

    :sparkles: add lint workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,45 @@
+// @ts-check
 import antfu from '@antfu/eslint-config'
 
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(antfu({
-
-  type: 'app',
-  vue: true,
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: false,
-    quotes: 'single'
-  }
-
-}, {
-  rules: {
-    'ts/consistent-type-definitions': ['error', 'type'], // use type instead of interface
-    'no-console': ['warn'], // allow console during development
-    'antfu/no-top-level-await': ['off'], // allow top level await
-    'node-prefer-global.process': ['off'],
-    'node/no-process-env': ['error'],
-    'perfectionist/sort-imports': ['error', {
-      tsconfigRootDir: '.'
-    }],
-    // file naming
-    'unicorn/filename-case': ['error', {
-      case: 'camelCase',
-      ignore: ['README.md']
-    }],
-    'style/comma-dangle': ['error', 'never']
-  }
-
-})
+export default withNuxt(
+  antfu(
+    {
+      type: 'app',
+      vue: true,
+      typescript: true,
+      formatters: true,
+      stylistic: {
+        indent: 2,
+        semi: false,
+        quotes: 'single'
+      }
+    },
+    {
+      ignores: [
+        '**/*.json',
+        '**/*.yaml',
+        '.pnpm-store/**',
+        'node_modules/**',
+        'dist/**'
+      ],
+      rules: {
+        'ts/consistent-type-definitions': ['error', 'type'], // use type instead of interface
+        'no-console': ['warn'], // allow console during development
+        'antfu/no-top-level-await': ['off'], // allow top level await
+        'node-prefer-global.process': ['off'],
+        'node/no-process-env': ['error'],
+        'perfectionist/sort-imports': ['error', {
+          tsconfigRootDir: '.'
+        }],
+        // file naming
+        'unicorn/filename-case': ['error', {
+          case: 'camelCase',
+          ignore: ['README.md']
+        }],
+        'style/comma-dangle': ['error', 'never']
+      }
+    }
+  )
 )
