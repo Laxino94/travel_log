22177ca17664314de8d9d3dd2485db0e3476e713
Laxino94 <huzehua2013@gmail.com>
2025-10-04 20:49:38 +0800

    feat(front page added): front page (#3)

---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Welcome to Travel Log, your personal travel companion. Document your journeys, share experiences,
+          and explore new destinations with ease.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
