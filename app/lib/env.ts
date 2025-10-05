67a5a3e0c95599cc56e9362ff3570894f8d531dd
Laxino94 <huzehua2013@gmail.com>
2025-10-05 21:12:26 +0800

    feat(auth): add auth (#9)

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -7,7 +7,9 @@ const EnvSchema = z.object({
   // to see if in development or production mode from node
   NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string()
+  TURSO_AUTH_TOKEN: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string()
 })
 
 tryParseEnv(EnvSchema)
