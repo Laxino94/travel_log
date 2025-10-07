da9c92177cd09647d861199b3fe2df869f1ef449
Laxino94 <huzehua2013@gmail.com>
2025-10-08 00:51:07 +0800

    feat(create ad location and protection): create a location and protection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ import './app/lib/env'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode', '@pinia/nuxt', '@vee-validate/nuxt', 'nuxt-csurf'],
   eslint: {
     config: {
       standalone: false // load config from .eslintrc (or package.json)
